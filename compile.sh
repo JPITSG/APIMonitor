22f977d21c204bd5ea0e96cdca37a91e4a82e6f2
JP IT <github@jpitsingapore.com>
2026-01-30 22:25:03 +0100

    Remove legacy compile.sh, superseded by Makefile

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-x86_64-w64-mingw32-windres resources.rc -o resources.o
-x86_64-w64-mingw32-gcc -O2 main.c resources.o -o APIMonitor.exe -lwinhttp -lshell32 -luser32 -lgdi32 -ladvapi32 -mwindows
